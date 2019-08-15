8f029237bf06a70840be8d4756d3422b3cac1b91
Ardavan Shafiee <bm.bigbang@gmail.com>
2019-08-15 04:02:50 +0100

    creating an add post request to add a single post given some userId

--- resolvers.js
+++ resolvers.js
@@ -3,6 +3,12 @@ module.exports = {
         getUser: () => null
     },
     Mutation: {
+        addPost: async(_parent, { title, imageUrl, categories, description, createdById }, { Post }) => {
+            return await new Post({
+                title, imageUrl, categories, description,
+                createdBy: createdById
+            }).save();
+        },
         signupUser: async (_parent, { username, email, password }, { User }) => {
             const user = await User.findOne({ username });
             if (user) throw new Error('User already exists');
