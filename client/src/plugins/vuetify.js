c1d3436ef8755787a1349bd86f5e0380806664b6
Ardavan Shafiee <bm.bigbang@gmail.com>
2019-08-16 06:19:56 +0100

    adding vuetify plugin file and autogenerated vue readme.md

---
+++ client/src/plugins/vuetify.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    icons: {
+        iconfont: 'mdi',
+    },
+    theme: { themes: {
+        light: {
+            primary: "#159648",
+            secondary: "#9F7E69",
+            accent: "#254441",
+            error: "#ff6f59",
+            warning: "#bcb382",
+            info: "#78909C",
+            success: "#388E3C"
+        }
+    }}
+});
