c2d51d3b2d8d26d155c712267f22c4f18963bc5a
Ardavan Shafiee <bm.bigbang@gmail.com>
2019-08-16 23:03:08 +0100

    fixing some issues with the icon loading and completing the nav bar component design.

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-toolbar fixed color="primary" dark>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-toolbar-title>
+      <v-toolbar-title class="hidden-xs-only">
         <router-link to="/" tag="span" style="cursor: pointer">
           Vue Photo Posts
         </router-link>
@@ -10,11 +10,37 @@
 
       <v-spacer></v-spacer>
       <v-toolbar-title tag="span">Search</v-toolbar-title>
-      <v-text-field flex placeholder="Search posts" style="margin-left: 12px"
+      <v-text-field flex prepend-icon="search" placeholder="Search posts" style="margin-left: 12px"
                     color="accent" single-line hide-details></v-text-field>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn color="primary" v-for="item in horizontalNavItems" :key="item.title" :to="item.link">
+          <v-icon class="hidden-sm-only" left>{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
     </v-toolbar>
     <main>
-      <router-view/>
+      <v-container class="mt-4">
+        <router-view/>
+      </v-container>
     </main>
   </v-app>
 </template>
+
+<script>
+  export default {
+    name: 'App',
+    computed: {
+      horizontalNavItems() {
+        return [
+          { icon: 'chat', title: 'Posts', link: '/posts' },
+          { icon: 'lock_open', title: 'Sign In', link: '/signin' },
+          { icon: 'create', title: 'Sign Up', link: '/signup'}
+        ]
+      }
+    }
+  }
+</script>
--- client/src/main.js
+++ client/src/main.js
@@ -5,11 +5,11 @@ import store from './store'
 import vuetify from './plugins/vuetify';
 import '@babel/polyfill'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
     router,
     store,
     vuetify,
     render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
