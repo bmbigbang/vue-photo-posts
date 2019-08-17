4434aea8dd98e7a4c908466fa27e090e98f86506
Ardavan Shafiee <bm.bigbang@gmail.com>
2019-08-17 23:55:44 +0100

    enabling vue-apollo and apollo-client plugins (installed in last commit) and transferring data through query schema resolution.

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,12 +1,35 @@
 <template>
   <v-app>
     <h1>Home</h1>
+    <ul v-for="post in getPosts" :key="post._id">
+      <li>
+        {{ post.title }}
+        {{ post.imageUrl }}
+        {{ post.description }}
+      </li>
+    </ul>
 <!--    <v-btn color="secondary">Button</v-btn>-->
   </v-app>
 </template>
 
 <script>
+import { gql } from 'apollo-boost';
+
 export default {
-  name: 'home'
+  name: 'home',
+  apollo: {
+    getPosts: {
+      query: gql`
+        query {
+          getPosts {
+            _id
+            title
+            imageUrl
+            description
+          }
+        }
+      `
+    }
+  }
 }
 </script>
