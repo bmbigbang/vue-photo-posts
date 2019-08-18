d656a66f985a44c5e46e8181ec577376bf3464b1
Ardavan Shafiee <bm.bigbang@gmail.com>
2019-08-18 12:19:03 +0100

    adding carousel for viewing of photo posts.

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,17 +1,13 @@
 <template>
-  <v-app>
-    <h1>Home</h1>
-    <div v-if="$apollo.loading">Loading...</div>
-    <ul v-else v-for="post in getPosts" :key="post._id">
-<!--      <h1>{{ (function(){debugger;post})() || "" }}</h1>-->
-      <li>
-        {{ post.title }}
-        {{ post.imageUrl }}
-        {{ post.description }}
-      </li>
-      <li>{{ post.likes }}</li>
-    </ul>
-<!--    <v-btn color="secondary">Button</v-btn>-->
+  <v-app v-if="getPosts">
+    <v-flex xs12>
+      <v-carousel v-bind="{ 'cycle': true }" interval="3000">
+        <v-carousel-item v-for="post in getPosts" :key="post._id" :src="post.imageUrl">
+<!--          <h1>{{ (function(){debugger;post})() || "" }}</h1>-->
+          <h1 id="carousel__title">{{ post.title }}</h1>
+        </v-carousel-item>
+      </v-carousel>
+    </v-flex>
   </v-app>
 </template>
 
@@ -52,3 +48,18 @@ export default {
   }
 }
 </script>
+
+<style>
+  #carousel__title {
+    position: absolute;
+    background-color: rgba(0, 0, 0, 0.5);
+    color: white;
+    border-radius: 5px 5px 0 0;
+    padding: 0.5em;
+    margin: 0 auto;
+    bottom: 50px;
+    left: 0;
+    right: 0;
+    text-align: center;
+  }
+</style>
