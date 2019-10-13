c923e049e104f4d78f9936cc149ae15cdab247a7
Ardavan Shafiee <bm.bigbang@gmail.com>
2019-10-13 19:09:32 +0100

    add UI elements for the profile page with details and favorite posts.

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -1,15 +1,66 @@
 <template>
-  <v-container text-center mt-6 pt-6>
-    <v-layout row wrap>
-      <v-flex xs12 sm6 offset-sm3>
-    <h1>Profile</h1>
+  <v-container text-xs-center mt-6 pt-6>
+    <v-flex sm6 offset-sm3>
+      <v-card class="white--text" color="secondary">
+        <v-layout row wrap>
+          <v-flex xs5>
+            <v-img height="125px" contain :src="user.avatar" id="user-avatar"></v-img>
+          </v-flex>
+          <v-flex xs7>
+            <v-card-title primary-title>
+              <div class="headline">{{ user.username }}</div>
+            </v-card-title>
+            <div>
+              <div>Joined {{ user.joinDate }}</div>
+              <div class="hidden-xs-only font-weight-thin">{{ user.favorites.length }} Favorites</div>
+              <div class="hidden-xs-only font-weight-thin">Posts Added</div>
+              <br/>
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-card>
+    </v-flex>
+
+    <v-container v-if="!userFavorites.length">
+      <v-layout row wrap text-center>
+        <v-flex xs12>
+          <h2>You have no favorites currently.</h2>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <v-container class="mt-3" v-else>
+      <v-flex xs12 text-center>
+        <h2 class="font-weight-light">
+          Favorites
+          <span class="font-weight-regular">{{ userFavorites.length }}</span>
+        </h2>
       </v-flex>
-    </v-layout>
+      <v-layout row wrap text-center>
+        <v-flex xs12 sm6 v-for="favorite in userFavorites" :key="favorite._id">
+          <v-card class="mt-3 ml-1 mr-2" hover>
+            <v-img height="30vh" :src="favorite.imageUrl"></v-img>
+            <v-card-text>{{ favorite.title }}</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-container>
 </template>
 
 <script>
-  export default {
-    name: "profile"
+import { mapGetters } from 'vuex'
+
+export default {
+  name: "profile",
+  computed: {
+    ...mapGetters(['user', 'userFavorites'])
   }
+}
 </script>
+
+<style>
+#user-avatar {
+  margin-top: 20px;
+}
+</style>
